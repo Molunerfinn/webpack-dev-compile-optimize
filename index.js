676797c7bf72b75c738038ccef2b1b6074af4e87
Molunerfinn <marksz@teamsz.xyz>
2017-06-07 13:48:12 +0800

    Fixed: semicolon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ WebpackDevCompileOptimizePlugin.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
     compilation.plugin(['optimize-chunks-basic', 'optimize-extracted-chunks-basic'], function(chunks) {
       chunks.forEach(function(chunk) {
-        chunk.parents = [];
+        chunk.parents = []
       })
     })
   })
